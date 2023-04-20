92356d7b2be4e4785bee1619394ad4c414b9dc63
DaudHidayatRamadhn <daud28ramadhan@gmail.com>
2023-04-20 20:41:43 +0700

    Belajar perulangan collection

--- tests/Feature/CollectionTest.php
+++ tests/Feature/CollectionTest.php
@@ -13,4 +13,13 @@ class CollectionTest extends TestCase
         $collection = collect([1,2,3]);
         $this->assertEqualsCanonicalizing([1,2,3], $collection->all());
     }
+    public function testForEach()
+    {
+        $collection = collect([1,2,3,4,5,6,7,8,9]);
+        foreach ($collection as $key => $value)
+        {
+        self::assertEquals($key+1, $value);
+        }
+
+    }
 }
