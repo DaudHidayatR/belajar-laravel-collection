a8013728f7dfa9044b7c593b573c9d83e6599a0d
DaudHidayatRamadhn <daud28ramadhan@gmail.com>
2023-04-20 21:59:48 +0700

    Belajar Mapping

--- tests/Feature/CollectionTest.php
+++ tests/Feature/CollectionTest.php
@@ -2,9 +2,11 @@
 
 namespace Tests\Feature;
 
+use App\Data\Person;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use function PHPUnit\Framework\assertEqualsCanonicalizing;
 
 class CollectionTest extends TestCase
 {
@@ -21,5 +23,64 @@ class CollectionTest extends TestCase
         self::assertEquals($key+1, $value);
         }
 
+    }
+    public function testCrud()
+    {
+        $collection = collect([]);
+        $collection->push(1,2,3);
+        assertEqualsCanonicalizing([1,2,3], $collection->all());
+
+        $result = $collection->pop();
+        assertEqualsCanonicalizing([1,2], $collection->all());
+    }
+    public function testMap()
+    {
+        $collection = collect([1,2,3]);
+        $result = $collection->map(function ($item){
+            return $item * 2;
+        });
+        assertEqualsCanonicalizing([2,4,6],$result->all());
+    }
+    public function testMapInto()
+    {
+        $collection = collect(['daud']);
+        $result = $collection->mapInto(Person::class);
+        $this->assertEqualsCanonicalizing([new Person("daud")], $result->all());
+    }
+    public function testMapSpread()
+    {
+        $collection = collect([['Daud','Hidayat'], ['Bintang', 'Rahmatullah']]);
+        $result = $collection->mapSpread(function ($firstName, $lastName){
+            $fullname = $firstName." ".$lastName;
+            return new Person($fullname);
+        });
+        self::assertEquals([
+            new Person("Daud Hidayat"),
+            new Person('Bintang Rahmatullah')
+        ], $result->all());
+    }
+    public function testMapGroups()
+    {
+        $collection = collect([
+            [
+                'name' => 'daud',
+                'department' => 'IT'
+            ],
+            [
+                'name' => 'Hidayat',
+                'department' => 'IT'
+            ],
+            [
+                'name' => 'Ramadhan',
+                'department' => 'HR'
+            ]
+        ]);
+        $result = $collection->mapToGroups(function ($item){
+            return[$item['department']=> $item['name']];
+        });
+        self::assertEquals([
+            'IT' => collect(['daud', 'Hidayat']),
+            'HR' => collect(['Ramadhan'])
+        ], $result->all());
     }
 }
