280409971eb2e8276037092355f34ede3f98b783
DaudHidayatRamadhn <daud28ramadhan@gmail.com>
2023-04-22 07:21:32 +0700

    Belajar Zipping

--- tests/Feature/CollectionTest.php
+++ tests/Feature/CollectionTest.php
@@ -83,4 +83,37 @@ class CollectionTest extends TestCase
             'HR' => collect(['Ramadhan'])
         ], $result->all());
     }
+    public function testZip()
+    {
+        $collection1 = collect([1,2,3]);
+        $collection2 = collect([4,5,6]);
+
+        $collection3 = $collection1->zip($collection2);
+
+        self::assertEquals([
+            collect([1,4]),
+            collect([2,5]),
+            collect([3,6])
+        ], $collection3->all());
+    }
+    public function testConcat()
+    {
+        $collection1 = collect([1,2,3]);
+        $collection2 = collect([4,5,6]);
+
+        $collection3 = $collection1->concat($collection2);
+
+        self::assertEquals([1,2,3,4,5,6], $collection3->all());
+    }
+    public function testCombine()
+    {
+        $collection1 = collect(['name', 'country']);
+        $collection2 = collect(['Daud', 'Indonesia']);
+        $collection3 = $collection1->combine($collection2);
+
+        $this->assertEqualsCanonicalizing([
+            'name' => 'Daud',
+            'country'=>'Indonesia'
+        ],$collection3->all());
+    }
 }
